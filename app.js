1eb4e269f0a3f77422e80659a1aba5858957e4d9
poojapatil-24 <poojapatil242016@gmail.com>
2025-08-03 19:05:24 +0530

    episode 2

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-const heading = React.createElement("h1", {id:'heading'}, "Hello world from React");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// React Element = It is a Object when we render on DOM it becomes html DOM element.
+
+const heading = React.createElement("h1", {id:'heading'}, "Namaste React!!");
+const jsxHeading = <h1 id="heading">Namaste React from JXS!!!</h1>
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(jsxHeading); //here react will replace from html DOM with current h1 tag.
+
+//So above code is very difficult to write, hence react developer written JSX.
+// JSX - is  not HTML inside js. IT has HTML like syntax.
+//Transpilation is done by parcel. parcel converts the code to ES6 before it reaches to the js.
